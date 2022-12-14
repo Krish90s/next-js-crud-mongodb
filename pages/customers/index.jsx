effadb22b21d681489cb888d745a953d5f60017e
Venkatakrishnan R <krishnanvenkat4@gmail.com>
2022-12-14 22:53:35 +0530

    updated ui

--- pages/customers/index.jsx
+++ pages/customers/index.jsx
@@ -1,7 +1,179 @@
+import { Fragment, useState } from "react";
+import { Dialog, Popover, Tab, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+const navigation = {
+  categories: [
+    {
+      id: "pay",
+      name: "Pay",
+      sections: [
+        {
+          id: "money_transfer",
+          name: "MONEY TRANSFER",
+          items: [
+            {
+              name: "UPI (Instant Mobile Money Transfer)",
+              href: "pay/money-transfer/upi",
+            },
+            {
+              name: "IMPS (Immediate Payment 24 * 7)",
+              href: "pay/money-transfer/imps",
+            },
+            {
+              name: "RTGS (Available 24 * 7)",
+              href: "pay/money-transfer/rtgs",
+            },
+            {
+              name: "NEFT (Available 24 * 7)",
+              href: "pay/money-transfer/neft",
+            },
+            {
+              name: "RemitNow (Forgie Outward Remittance)",
+              href: "pay/money-transfer/remit-now",
+            },
+            {
+              name: "RemitNow (Forgie Outward Remittance)",
+              href: "pay/money-transfer/remit-now",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const Customers = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <h1>Customers</h1>
+      <div className="bg-white">
+        <header className="relative bg-white">
+          <nav
+            aria-label="Top"
+            className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+          >
+            <div className="border-b border-gray-200">
+              <div className="flex h-16 items-center">
+                {/* Logo */}
+                <div className="ml-4 flex lg:ml-0">
+                  <Link href="/">
+                    <span className="sr-only">E-Banking</span>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+
+                {/* Flyout menus */}
+                <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
+                  <div className="flex h-full space-x-8">
+                    {navigation.categories.map((category) => (
+                      <Popover key={category.name} className="flex">
+                        {({ open }) => (
+                          <>
+                            <div className="relative flex">
+                              <Popover.Button
+                                className={classNames(
+                                  open
+                                    ? "border-indigo-600 text-indigo-600"
+                                    : "border-transparent text-gray-700 hover:text-gray-800",
+                                  "relative z-10 -mb-px flex items-center border-b-2 pt-px text-sm font-medium transition-colors duration-200 ease-out"
+                                )}
+                              >
+                                {category.name}
+                              </Popover.Button>
+                            </div>
+
+                            <Transition
+                              as={Fragment}
+                              enter="transition ease-out duration-200"
+                              enterFrom="opacity-0"
+                              enterTo="opacity-100"
+                              leave="transition ease-in duration-150"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
+                                {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                                <div
+                                  className="absolute inset-0 top-1/2 bg-white shadow"
+                                  aria-hidden="true"
+                                />
+
+                                <div className="relative bg-white">
+                                  <div className="mx-auto max-w-7xl px-8">
+                                    <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
+                                      <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
+                                        {category.sections.map((section) => (
+                                          <div key={section.name}>
+                                            <p
+                                              id={`${section.name}-heading`}
+                                              className="font-medium text-gray-900"
+                                            >
+                                              {section.name}
+                                            </p>
+                                            <ul
+                                              role="list"
+                                              aria-labelledby={`${section.name}-heading`}
+                                              className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
+                                            >
+                                              {section.items.map((item) => (
+                                                <li
+                                                  key={item.name}
+                                                  className="flex"
+                                                >
+                                                  <Link
+                                                    href={item.href}
+                                                    className="hover:text-gray-800"
+                                                  >
+                                                    {item.name}
+                                                  </Link>
+                                                </li>
+                                              ))}
+                                            </ul>
+                                          </div>
+                                        ))}
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </Popover.Panel>
+                            </Transition>
+                          </>
+                        )}
+                      </Popover>
+                    ))}
+                  </div>
+                </Popover.Group>
+
+                <div className="ml-auto flex items-center">
+                  <button
+                    type="button"
+                    className="rounded-md bg-gray-800 p-2 text-gray-400 lg:hidden"
+                    onClick={() => setOpen(true)}
+                  >
+                    <span className="sr-only">Open menu</span>
+                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
     </>
   );
 };
